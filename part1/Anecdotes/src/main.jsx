e08b58e5e279e1af4d8c71ce1a84a551261100a6
Nikki Vaughan <vnikki13@gmail.com>
2025-01-27 14:18:08 -0500

    Complete Part1: Anecdote exercises

---
+++ part1/Anecdotes/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import App from './App'
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
